99c235c1b9d3b4cee4e9220b128b2c7ee65e9147
Riccardo Calvi <riccardoclv@gmail.com>
2022-10-31 12:59:07 +0100

    d1 => Consegna Esercizio

---
+++ m1/w2/d1/src/scripts/script.js
@@ -0,0 +1,14 @@
+// Commento su riga
+
+/* Commento
+su più righe
+*/
+
+var anni = prompt('Quanti anni hai?')
+console.log(anni)
+
+if (anni >= 18) {
+    document.write("<br>Sei Maggiorenne")
+} else {
+    document.write("<br>Sei Minorenne")
+}
