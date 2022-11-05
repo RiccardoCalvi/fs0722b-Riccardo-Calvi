729e530ab45735690c0ccf2eecb6b7a2d2d67a5a
Riccardo Calvi <riccardoclv@gmail.com>
2022-11-05 10:36:32 +0100

    d5 => Aggiunte funzioni di reset e cancellazione

--- m1/w2/d5/src/scripts/script.js
+++ m1/w2/d5/src/scripts/script.js
@@ -1,10 +1,18 @@
-function aggiungiCifra(bottone){
+var display = document.getElementById("display")
 
-  document.getElementById("display").value += bottone.getAttribute('data-simbolo');
-
+function aggiungiCifra(bottone) {
+  display.value += bottone.getAttribute("data-simbolo");
 }
 
-function tot(){
-  let numeri = document.getElementById("display");
+function tot() {
+  let numeri = display;
   numeri.value = eval(numeri.value);
+}
+
+function cancella() {
+  display.value = display.value.substr(0, display.value.length - 1);
+}
+
+function reset() {
+  display.value = "";
 }
