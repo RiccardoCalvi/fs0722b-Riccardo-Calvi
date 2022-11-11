4aef482d470e118ed56986e2331aa7a48b8556be
Riccardo Calvi <riccardoclv@gmail.com>
2022-11-11 13:32:22 +0100

    d5 => consegna esercizio 12

--- m1/w3/d5/src/scripts/script.js
+++ m1/w3/d5/src/scripts/script.js
@@ -376,11 +376,17 @@ function newestMovie() {
   return out;
 }
 
-console.log(newestMovie());
+// console.log(newestMovie());
 
 /* ESERCIZIO 13
   Scrivi una funzione chiamata countMovies che ritorna il numero di film contenuti nell'array "movies" fornito.
 */
+
+function countMovies() {
+  return movies.length
+}
+
+// console.log(countMovies())
 
 /* ESERCIZIO 14
   Scrivi una funzione chiamata "onlyTheYears" che crea un array con solamente gli anni di uscita dei film contenuti nell'array "movies" fornito.
