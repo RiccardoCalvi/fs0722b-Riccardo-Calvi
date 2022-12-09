df24b3ee445c90ebdcd4d55a822c0b2b51920a1a
Riccardo Calvi <riccardoclv@gmail.com>
2022-12-09 09:50:26 +0100

    D3 => Modifiche esercizio

--- m2/w7/d3/contact.js
+++ m2/w7/d3/contact.js
@@ -2,16 +2,31 @@ let lista = document.querySelector("#listaUtenti");
 let inputUser = document.querySelector("#nuovoUtente");
 let aggiungi_btn = document.querySelector("#aggiungi");
 let users = [];                             // array vuoto
+let backup = localStorage.getItem("utenti")
+console.log(backup)
+
+if (backup) {
+    backup = backup.split(",");
+    backup = JSON.parse(backup)
+    console.log(backup)
+    backup.forEach(element => {
+        users.push(element)
+    });  
+    console.log(users)
+}
+
+
 
 aggiungi_btn.addEventListener("click", () => {
     let usr = new User(inputUser.value);    // è l'oggetto che definisco a riga 34
-    users.push(usr);                        // push nell'array
+    users.push(usr);
+    localStorage.setItem("utenti", JSON.stringify(users))
     writelnList();                          // lancio la funzione
 });
 
 function writelnList() {
     lista.innerHTML = "";
-    users.forEach((el, index) => {    // parametri di for each(valore corrente, indice, array)
+    users.forEach((el) => {    // parametri di for each(valore corrente, indice, array)
         lista.innerHTML += (`<li>${el.logUser()}</li>`); // utilizzo il metodo dell'oggetto sotto
     })
     console.log(users);
@@ -26,4 +41,14 @@ class User {
     logUser() {
         return `<b>${this.name}</b>`;
     }
+}
+
+
+function resetList() {
+    localStorage.clear("utenti")
+
+}
+function removeList() {
+    localStorage.setItem("utenti", "")
+
 }
