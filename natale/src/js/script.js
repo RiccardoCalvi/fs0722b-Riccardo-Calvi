a54588dbe4752f51b1ef767a5ef6231cd0eb71c0
Riccardo Calvi <riccardoclv@gmail.com>
2022-12-30 02:57:07 +0100

    Natale => Aggiunto fetch forme + costruzione classe

--- natale/src/js/script.js
+++ natale/src/js/script.js
@@ -1,11 +1,26 @@
+var url = "https://raw.githubusercontent.com/RiccardoCalvi/fs0722b-Riccardo-Calvi/main/natale/api/shape.json";
+var requestOptions = { method: 'GET', redirect: 'follow' };
 var forme = [];
 var Forma = /** @class */ (function () {
     function Forma(istruzioni) {
         this.id = istruzioni.id;
+        this.nome = istruzioni.nome;
         this.altezza = istruzioni.altezza;
         this.lunghezza = istruzioni.lunghezza;
     }
+    Forma.prototype.calcolaArea = function () {
+        return this.altezza * this.lunghezza;
+    };
     return Forma;
 }());
-var quadrato = new Forma();
-console.log(quadrato);
+fetch(url, requestOptions)
+    .then(function (response) { return response.json(); })
+    .then(function (result) {
+    result.forEach(function (elemento) {
+        forme.push(new Forma(elemento));
+    });
+    console.log(forme);
+    forme.forEach(function (elemento) {
+        console.log(elemento.calcolaArea());
+    });
+});
--- natale/src/ts/script.ts
+++ natale/src/ts/script.ts
@@ -1,3 +1,5 @@
+const url = "https://raw.githubusercontent.com/RiccardoCalvi/fs0722b-Riccardo-Calvi/main/natale/api/shape.json"
+const requestOptions: object = { method: 'GET', redirect: 'follow' };
 let forme = []
 
 class Forma {
@@ -5,15 +7,33 @@ class Forma {
     altezza: number;
     lunghezza: number;
     profondita: number;
+    nome: string;
 
     constructor(istruzioni) {
         this.id = istruzioni.id;
+        this.nome = istruzioni.nome
         this.altezza = istruzioni.altezza;
         this.lunghezza = istruzioni.lunghezza;
     }
 
+    calcolaArea(){
+        return this.altezza * this.lunghezza
+    }
+
 }
 
-const quadrato = new Forma()
 
-console.log(quadrato)
+
+  
+fetch(url, requestOptions)
+.then(response => response.json())
+.then(result => {
+    
+    result.forEach(elemento => {
+        forme.push(new Forma(elemento))
+    });
+    console.log(forme)
+    forme.forEach(elemento => {
+        console.log(elemento.calcolaArea())
+    });
+})
